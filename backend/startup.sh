6084d8557451aaf515e76abe8c0e82ced891acbf
LeeSeungJae <eaios@LeeSeungJaeui-MacBookAir.local>
2025-06-01 17:23:36 +0900

    fix: db migration

--- backend/startup.sh
+++ backend/startup.sh
@@ -6,8 +6,8 @@ while ! pg_isready -h db -U brain -d brainshare; do
   sleep 1
 done
 
-# echo "Running alembic upgrade head..."
-# poetry run alembic upgrade head
+echo "Running alembic upgrade head..."
+poetry run alembic upgrade head
 
 echo "Starting server..."
 poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
